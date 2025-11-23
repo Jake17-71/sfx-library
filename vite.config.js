05e2b9520d921fa380a26988213adb12ce47324f
Dan Arhipov <chifmatsuno17@gmail.com>
2025-11-23 17:33:20 +0300

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  base: '/sfx-library/',
+  build: {
+    outDir: 'docs',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
+})
